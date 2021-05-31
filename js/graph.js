8c99d8fc2bd71cd5801cbef848f5729a0913069a
Tibor Harcsa <silurust@gmail.com>
2021-06-01 01:25:14 +0200

    in ruins now

--- js/graph.js
+++ js/graph.js
@@ -5,98 +5,60 @@ class Graph {
 	this.nu_edges = 0;
         this.ns = [];
 	this.sn = {};
-	//this.vertnames = [];
 	this.adj = {};
-	this.badj = {};
 	this.path = [];
 	this.path_col = "#ff00ff";
     }
-    addNode(node) {
+    add_vertex(node) {
         this.ns.push(node);
 	this.sn[node.name] = node;
 	this.adj[this.nu_vertices] = [];
-	this.badj[this.nu_vertices] = [];
 	++this.nu_vertices;
         return this.ns.length-1;
     }
-    remNodeByName(name) {
+    rem_vertex_by_name(name) {
 	if(this.sn[name]===undefined)
 	    return false;
-	if (this.remNode(this.ns.indexOf(this.sn[name])))
+	if (this.rem_vertex(this.ns.indexOf(this.sn[name])))
 	    return true;
 	return false;
     }
-    remNode(ind) {
-	if(ind < 0)
+    rem_vertex(ind) {
+	if(ind < 0 || this.nu_vertices <= ind)
 	    return false;
 	const the_node = this.ns[ind];
-	for(let i = the_node.links.length-1; i >= 0; --i) {
-	    the_node.disconnect(the_node.links[i]);
-	    --this.nu_edges;
-	}
-	for(let i = the_node.backLinks.length-1; i >= 0; --i) {
-	    the_node.backLinks[i].disconnect(the_node);
-	    --this.nu_edges;
-	}
 	delete this.sn[this.ns[ind].name];
 	this.ns.splice(ind,1);
-	this.remLinks(ind,this.adj,this.badj);
-	this.remLinks(ind,this.badj,this.adj);
+	this.remLinks(ind,this.adj);
 	delete this.adj[ind];
-	delete this.badj[ind];
 	--this.nu_vertices;
 	return true;
     }
-    addLink(index1, index2) {
-        if (index1<0 || index1>=this.ns.length || index2<0 || index2>=this.ns.length)
-            return false;
-	if (!this.is_connected(index1,index2) && index1 != index2) {
-            this.ns[index1].connect(this.ns[index2]);
-	    ++this.nu_edges;
-	    this.adj[index1].push(index2);
-	    this.badj[index2].push(index1);
-	    return true;
-	}
-	return false;
+    add_edge(v1,v2) {
+	this.add_uni_edge(v1,v2);
+	if (v1 != v2)
+	    this.add_uni_edge(v2,v1);
     }
-    remLink(index1, index2) {
-	if (!this.is_connected(index1,index2))
-	    return false;
-	if (this.ns[index1].disconnect(this.ns[index2]))
-	    --this.nu_edges;
-	this.remLink2(index1,index2);
+    rem_edge(v1,v2) {
+	this.rem_uni_edge(v1,v2);
+	if (v1 != v2)
+	    this.rem_uni_edge(v2,v1);
     }
-    remLinks(ind1,adj1,adj2) {
-	for(let ind2 of adj1[ind1])
-	    adj2[ind2].splice(adj2[ind2].indexOf(ind1),1);
+    add_uni_edge(v1,v2) {
+	this.adj[v1].push(v2);
+	++this.nu_edges;
     }
-    remLink2(index1,index2) {
-	const ind_to_remove1 = this.adj[index1].indexOf(index2);
-	if(ind_to_remove1 > -1)
-	    this.adj[index1].splice(ind_to_remove1,1);
-	else {
-	    console.log("no link between " + index1 + " and " + index2);
-	    return false;
-	}
-	const ind_to_remove2 = this.badj[index2].indexOf(index1);
-	if(ind_to_remove2 > -1)
-	    this.badj[index2].splice(ind_to_remove2,1);
-	else {
-	    console.log("no link between " + index2 + " and " + index1);
-	    return false;
-	}
-	return true;
+    rem_uni_edge(v1,v2) {
+	this.adj[v1].splice(this.adj[v1].indexOf(v2),1);
+	--this.nu_edges;
     }
     reposition_node(num,xpos,ypos) {
 	this.ns[num].x = xpos;
 	this.ns[num].y = ypos;
     }
     is_connected(index1,index2) {
-	return(Node.is_connected(this.ns[index1],this.ns[index2]));
+	return(this.adj[index1].indexOf[index2] > -1 || this.adj[index2].indexOf[index1] > -1);
     }
-    // add_uni_link(index1, index2) {
-    // 	this.ns[index1].add_uni_link(this.ns[index2]);
-    // }
     draw(params,draw_trace,draw_labels) {
 	//console.log("graph.draw() has been called");
         this.ns.forEach( node => {
